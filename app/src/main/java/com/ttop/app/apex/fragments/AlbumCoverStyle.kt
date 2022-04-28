e0eff4838add1f5b9e223e369f78fb9d49e31ed5
TheTerminatorOfProgramming <41417119+TheTerminatorOfProgramming@users.noreply.github.com>
2022-04-28 13:05:23 +0800

    Update AlbumCoverStyle.kt

--- app/src/main/java/com/ttop/app/apex/fragments/AlbumCoverStyle.kt
+++ app/src/main/java/com/ttop/app/apex/fragments/AlbumCoverStyle.kt
@@ -23,10 +23,10 @@ enum class AlbumCoverStyle(
     @DrawableRes val drawableResId: Int,
     val id: Int
 ) {
-    Card(R.string.card, R.drawable.np_blur_card, 3),
-    Circle(R.string.circular, R.drawable.np_circle, 2),
-    Flat(R.string.flat, R.drawable.np_flat, 1),
-    FullCard(R.string.full_card, R.drawable.np_adaptive, 5),
-    Full(R.string.full, R.drawable.np_full, 4),
-    Normal(R.string.normal, R.drawable.np_normal, 0),
+    Card(R.string.card, R.drawable.player_blur_card, 3),
+    Circle(R.string.circular, R.drawable.player_circle, 2),
+    Flat(R.string.flat, R.drawable.player_flat, 1),
+    FullCard(R.string.full_card, R.drawable.player_adaptive, 5),
+    Full(R.string.full, R.drawable.player_full, 4),
+    Normal(R.string.normal, R.drawable.player_normal, 0),
 }
